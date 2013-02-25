c08cce154ff0d13d16e93410bc12e15b245e9439
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2013-02-25 11:30:50 -0800

    init

---
+++ lib/CPAN/Test/Dummy/FromGit.pm
@@ -0,0 +1,41 @@
+package CPAN::Test::Dummy::FromGit;
+
+use strict;
+use 5.008_001;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+CPAN::Test::Dummy::FromGit -
+
+=head1 SYNOPSIS
+
+  use CPAN::Test::Dummy::FromGit;
+
+=head1 DESCRIPTION
+
+CPAN::Test::Dummy::FromGit is
+
+=head1 AUTHOR
+
+Tatsuhiko Miyagawa E<lt>miyagawa@bulknews.netE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013- Tatsuhiko Miyagawa
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
